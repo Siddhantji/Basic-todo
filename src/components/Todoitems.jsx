59507b9178a85ac07c01a9a4521935e3c2e3b6cf
Siddhantji <teotiasiddhant@gmail.com>
2024-06-20 00:34:56 +0530

    gave module css to component

--- src/components/Todoitems.jsx
+++ src/components/Todoitems.jsx
@@ -1,8 +1,8 @@
 import TodoItem from "./TodoItem";
-
+import style from "./Todoitems.module.css";
 function Todoitems({ tasks }) {
   return (
-    <div className="items-container">
+    <div className={style.itemsContainer}>
       {tasks.map((element) => (
         <TodoItem todoName={element.name} todoDate={element.date} />
       ))}
